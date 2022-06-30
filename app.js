9be6bb64ee9f58f941d2aa3e18173dcd04680116
LauryF <laury136@hotmail.fr>
2022-06-30 19:37:37 +0200

    Middleware + routes GET et POST

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
 // Pour IMPORTER 'Express'
 const express = require('express');
 
-// Pour CREER un application (actuellement vide)
+// Pour CREER une application* (actuellement vide) (*l'application contient le serveur)
 const app = express();
 
-// Pour RENVOYER une réponse (en format 'json') à la requête envoyée
-app.use((req, res) => {
-    res.json({message : 'Votre requête a bien été reçue !'})
+// Pour INTERCEPTER toutes les requêtes qui contiennent du json et METTRE à disposition ce contenu (ACCEDER au corps de la requête) sur l'objet 'requête' (dans req.body)
+// (identique à 'body-parser')
+app.use(express.json());
+
+// Pour IMPLEMENTER "CORS" (appels sécurisés vers l'application) (pas d'URL précisée pour permettre une application à toutes les routes)
+app.use((req, res, next) => {
+    // Pour accéder à l'API depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Pour ajouter les headers mentionnés aux requêtes envoyées vers l'API
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    // Pour envoyer des requêtes avec différentes méthodes
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// * Mentorat : Voir ensemble les 2 méthodes
+
+// Méthode 2 : Laisser 'use' et insérer ce middleware : (?)
+app.post('/api/stuff', (req, res, next) => {
+    // Pour AFFICHER le corps de la requête
+    console.log(req.body);
+    // Pour ENVOYER la réponse (à la requête (+ PRECISER le status (que l'on souhaite) de la réponse)
+    res.status(201).json({ message: 'Objet créé !' }); // 201 : création de ressources (dans la BdD)
+});
+
+// L'argument "/api/stuff" est l'URL visée par l'application ("endpoint" = route vers l'API)
+// Méthode 1 : REMPLACER 'use' par 'get' (pour RECUPERER seulement (CIBLER) les requêtes 'get') (idem avec 'post') : (?)
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(stuff);
 });
 
 // Pour EXPORTER l'application
--- server.js
+++ server.js
@@ -1,13 +1,78 @@
 // Pour IMPORTER le package http de Node
 const http = require('http');
-// Pour importer l'application
+// Pour IMPORTER l'application
 const app = require('./app');
 
-// Pour INDIQUER à l'application Express sur quel port elle va tourner
-app.set('port' , process.env.PORT || 3000);
+// Pour RENVOYER un port valide (qu'il soit fourni sous la forme d'un numéro ou d'une chaîne)
+const normalizePort = val => {
+    const port = parseInt(val, 10);
 
-// Pour PASSER l'application au serveur (app = fonction qui va recevoir la requête et la réponse et qui va apporter des modifications)
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+
+/* // Pour INDIQUER à l'application Express sur quel port elle va tourner
+app.set('port' , process.env.PORT || 3000); */
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// Pour RECHERCHER les différentes erreurs et les GERER de manière appropriée (elle est ensuite enregistrée dans le serveur)
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+// Pour PASSER l'application au serveur (en le CREANT) ('app' = fonction qui va recevoir la requête et la réponse et qui va apporter des modifications)
 const server = http.createServer(app);
 
-// ECOUTE du serveur (des requêtes envoyées)
-server.listen(process.env.PORT || 3000);
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+});
+
+/* // Pour ECOUTER le serveur (des requêtes envoyées)
+server.listen(process.env.PORT || 3000); */
+
+// (écouteur d'évènements consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console)
+server.listen(port);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
